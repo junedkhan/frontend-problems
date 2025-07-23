fa7810d2d66d04034e542f650f51e800efe58b24
Juned Khan <junedkhan@Juneds-MacBook-Air.local>
2025-07-23 13:42:26 +0530

    added the problem find the second largest

---
+++ typescript/findTheSecondLargest.ts
@@ -0,0 +1,20 @@
+// Find the secod largest elemnt from an array
+
+function findTheSecondLargest(arr: number[]): number {
+    let largest = arr[0];
+    let secondLargest = -Infinity;
+    for(let i = 1; i < arr.length; i++) {
+        if(arr[i] === largest) continue;
+        if(arr[i] > largest) {
+            secondLargest = largest;
+            largest = arr[i];
+ 
+        }  else if(arr[i] > secondLargest) {
+           secondLargest = arr[i];
+        }
+    }
+ 
+    return secondLargest;
+ }
+ 
+ console.log(findTheSecondLargest([4, 5,1, 4, 7, 4, 6]));
