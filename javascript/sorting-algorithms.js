f783394990786f529a64f697531f40832282581d
Juned Khan <junedkhan@Juneds-MacBook-Air.local>
2025-07-23 13:43:50 +0530

    added the problem find the second largest

--- javascript/sorting-algorithms.js
+++ javascript/sorting-algorithms.js
@@ -1,14 +1,15 @@
+"use strict";
 function swap(arr, i, j) {
-    var temp = arr[i];
+    const temp = arr[i];
     arr[i] = arr[j];
     arr[j] = temp;
 }
 // Select the minimum => 
 function selectionSort(arr) {
-    var n = arr.length;
-    for (var i = 0; i < n - 1; i++) {
-        var minimum = i;
-        for (var j = i; j < n; j++) {
+    const n = arr.length;
+    for (let i = 0; i < n - 1; i++) {
+        let minimum = i;
+        for (let j = i; j < n; j++) {
             if (arr[j] < arr[minimum]) {
                 minimum = j;
             }
@@ -19,10 +20,10 @@ function selectionSort(arr) {
 }
 // bubble Sort => move the bigger element to the last
 function bubbleSort(arr) {
-    var n = arr.length;
-    for (var i = n - 1; i >= 0; i--) {
-        var didiSwap = 0;
-        for (var j = 0; j < i; j++) {
+    const n = arr.length;
+    for (let i = n - 1; i >= 0; i--) {
+        let didiSwap = 0;
+        for (let j = 0; j < i; j++) {
             if (arr[j] > arr[j + 1]) {
                 swap(arr, j, j + 1);
                 didiSwap = 1;
@@ -36,9 +37,9 @@ function bubbleSort(arr) {
 }
 // InsertionSort => 
 function insertionSort(arr) {
-    var n = arr.length;
-    for (var i = 0; i < n; i++) {
-        var j = i;
+    const n = arr.length;
+    for (let i = 0; i < n; i++) {
+        let j = i;
         while (j > 0 && arr[j] < arr[j - 1]) {
             swap(arr, j, j - 1);
             j--;
@@ -47,9 +48,9 @@ function insertionSort(arr) {
     return arr;
 }
 function merge(arr, low, mid, high) {
-    var first = low;
-    var second = mid + 1;
-    var temp = [];
+    let first = low;
+    let second = mid + 1;
+    let temp = [];
     while (first <= mid && second <= high) {
         if (arr[first] < arr[second]) {
             temp.push(arr[first]);
@@ -68,23 +69,23 @@ function merge(arr, low, mid, high) {
         temp.push(arr[second]);
         second++;
     }
-    for (var i = low; i <= high; i++) {
+    for (let i = low; i <= high; i++) {
         arr[i] = temp[i - low]; // 0 4 => 4 - 4 => 0
     }
 }
 function mergeSort(arr, low, high) {
     if (low === high)
         return;
-    var mid = Math.floor((low + high) / 2);
+    const mid = Math.floor((low + high) / 2);
     mergeSort(arr, low, mid);
     mergeSort(arr, mid + 1, high);
     merge(arr, low, mid, high);
 }
 //Quick Sort 
 function findPivotIndex(arr, low, high) {
-    var pivot = arr[low];
-    var i = low;
-    var j = high;
+    const pivot = arr[low];
+    let i = low;
+    let j = high;
     while (i < j) {
         while (arr[i] <= pivot && i < high) {
             i++;
@@ -100,13 +101,13 @@ function findPivotIndex(arr, low, high) {
 }
 function quickSort(arr, low, high) {
     if (low <= high) {
-        var pivotIndex = findPivotIndex(arr, low, high);
+        const pivotIndex = findPivotIndex(arr, low, high);
         quickSort(arr, low, pivotIndex - 1);
         quickSort(arr, pivotIndex + 1, high);
     }
 }
-var arr1 = [7, 4, 1, 5, 3];
-var arr2 = [5, 4, 4, 1, 1];
+const arr1 = [7, 4, 1, 5, 3];
+const arr2 = [5, 4, 4, 1, 1];
 quickSort(arr1, 0, 4);
 quickSort(arr2, 0, 4);
 console.log("test 1", arr1);
